eed138d3d65519132c4154ac2f018142cddc0c6f
Santiago Valencia Díaz <santiago.valencia6@utp.edu.co>
2024-10-27 19:24:34 -0500

    Subiendo proyecto

---
+++ src/app/modules/layout/components/navbar/navbar.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import {
+  faBell,
+  faInfoCircle,
+  faClose,
+  faAngleDown
+} from '@fortawesome/free-solid-svg-icons';
+
+import { AuthService } from '@services/auth.service';
+import { TokenService } from '@services/token.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+})
+export class NavbarComponent {
+  faBell = faBell;
+  faInfoCircle = faInfoCircle;
+  faClose = faClose;
+  faAngleDown = faAngleDown;
+
+  isOpenOverlayAvatar = false;
+  isOpenOverlayBoards = false;
+
+  user$ = this.authService.user$;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private tokenService: TokenService
+  ) {}
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+
+  isValidToken() {
+    console.log(this.tokenService.isValidToken());
+  }
+}
